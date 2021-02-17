97ba9159d8aa40ef137ee0c18336c76264f98727
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-16 19:21:41 -0600

    progress

---
+++ README.md
@@ -0,0 +1,6 @@
+# wasm-bind-demo
+
+This demonstrates using wasm-bind to call Rust functions from JavaScript.
+
+Due to this issue, webpack 5 cannot yet be used with wasm-pack.
+https://github.com/rustwasm/wasm-pack/issues/835
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // A dynamic import is required due to the issue webpack/webpack#6615,
-// but in theory `import { greet } from './pkg';` will work eventually.
+// but in theory `import {greet, sum} from './pkg';` will work eventually.
 const rust = import('./pkg');
 
 /*
@@ -13,13 +13,35 @@ rust
   .catch(console.error);
 */
 
+function getNumbers(count) {
+  const numbers = [];
+  for (let i = 0; i < count; i++) {
+    numbers.push(Math.random() * 100);
+  }
+  return numbers;
+}
+
+function sum(numbers) {
+  return numbers.reduce((acc, n) => acc + n);
+}
+
 async function run(m) {
   try {
     const m = await rust;
+
     //m.greet('World!');
-    const arr = new Float64Array([1.2, 2.3, 3.4]);
-    const sum = m.sum(arr);
-    console.log('index.js: sum =', sum);
+
+    const arr = new Float64Array(getNumbers(10000000));
+
+    let startMs = Date.now();
+    let result = sum(arr);
+    let endMs = Date.now();
+    console.log('JavaScript sum =', result, 'in', endMs - startMs, 'ms');
+
+    startMs = Date.now();
+    result = m.sum(arr);
+    endMs = Date.now();
+    console.log('Rust sum =', result, 'in', endMs - startMs, 'ms');
   } catch (e) {
     console.error(e);
   }
