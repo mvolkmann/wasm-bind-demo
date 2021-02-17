969cc2857659eb6d6fefb8cae54c78d59c831e42
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-16 18:46:03 -0600

    progress

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
-// will work here one day as well!
+// A dynamic import is required due to the issue webpack/webpack#6615,
+// but in theory `import { greet } from './pkg';` will work eventually.
 const rust = import('./pkg');
 
+/*
 rust
   .then(m => {
     //m.greet('World!');
@@ -11,3 +11,18 @@ rust
     console.log('index.js: sum =', sum);
   })
   .catch(console.error);
+*/
+
+async function run(m) {
+  try {
+    const m = await rust;
+    //m.greet('World!');
+    const arr = new Float64Array([1.2, 2.3, 3.4]);
+    const sum = m.sum(arr);
+    console.log('index.js: sum =', sum);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+run();
